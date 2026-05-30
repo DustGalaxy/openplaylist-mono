b9b64f27e67d7b2a0f60e60c4f38a3e9cd0b6495
DustGalaxy <andreinter903@gmail.com>
2026-05-30 19:03:46 +0300

    dist

---
+++ new_ui/dist/assets/input-ZJgJstEy.js
@@ -0,0 +1,31 @@
+import{j as t,a7 as o}from"./index-BAu9KRFU.js";function d({text:e,onClick:a,className:r,disabled:s=!1,...i}){return t.jsx("button",{onClick:a,disabled:s,className:o(`
+        pt-0.5 pb-[3px]            
+        sm:pt-1 sm:pb-[5px] 
+        cursor-pointer 
+        transition-all 
+        duration-100 
+        ease-out
+        rounded-[var(--rounded-std)] 
+        flex items-center justify-center 
+        box-border
+        
+        shadow-[0_3px_0_0_theme(colors.level-3),_0_0px_10px_rgba(0,0,0,0.4),_0_2px_4px_rgba(0,0,0,0.3)]
+        sm:shadow-[0_5px_0_0_theme(colors.level-3),_0_0px_15px_rgba(0,0,0,0.55),_0_4px_8px_rgba(0,0,0,0.45)]
+        
+        hover:text-shadow-[0_0_4px_rgba(255,255,255,0.8),_0_0_25px_rgba(255,255,255,0.4)]
+        hover:[&_svg]:drop-shadow-[0_0_4px_rgba(255,255,255,0.8)]
+        disabled:cursor-not-allowed
+        disabled:opacity-50
+        disabled:shadow-none
+        disabled:hover:shadow-none
+        disabled:hover:text-shadow-none
+        disabled:[&_svg]:drop-shadow-none
+        disabled:active:shadow-none
+        disabled:active:translate-y-0
+        transform translate-y-0
+        active:translate-y-[3px]
+        sm:active:translate-y-[5px]   
+        
+        active:shadow-[0_0px_0_0_theme(colors.level-3),0_0px_3px_rgba(0,0,0,0.1),0_1px_2px_rgba(0,0,0,0.05)] 
+        
+      `,r),...i,children:e})}function l({className:e,type:a,...r}){return t.jsx("input",{type:a,"data-slot":"input",className:o("file:text-foreground placeholder:text-text-secondary dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm","focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]","aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",e),...r})}export{d as B,l as I};
