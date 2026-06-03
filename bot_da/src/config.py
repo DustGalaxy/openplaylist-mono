1ca5c7e31fe5a6e04b5f5de19c4f76cb3ce9799d
DustGalaxy <andreinter903@gmail.com>
2026-06-03 14:33:19 +0300

    bot_da fix

--- bot_da/src/config.py
+++ bot_da/src/config.py
@@ -1,12 +1,13 @@
+from pydantic import model_validator
 from pydantic_settings import BaseSettings  # Для загрузки конфига
 
-
+PROJECT_DOMAIN: str = "http://localhost:3000"
 # --- Загрузка конфигурации ---
 class Settings(BaseSettings):
-    APP_ID: str
-    API_KEY: str
-    REDIRECT_URI: str
-    SESSION_SECRET_KEY: str
+    APP_ID: str = "18779"
+    API_KEY: str = "oauth-user-show oauth-donation-subscribe"
+    REDIRECT_URI: str = ""
+    SESSION_SECRET_KEY: str 
 
     RABBITMQ_URL: str
     DB_URL: str
@@ -18,5 +19,14 @@ class Settings(BaseSettings):
     DA_API_BASE_URL: str = "https://www.donationalerts.com/api/v1"
     DA_CENTRIFUGO_URL: str = "wss://centrifugo.donationalerts.com/connection/websocket"
 
+    @model_validator(mode="after")
+    def compute_urls(self) -> "Settings":
+        # Если в .env написано MODE=prod, меняем домен (или подтягиваем из другой переменной PROD_PROJECT_DOMAIN)
+
+        self.PROJECT_DOMAIN = "https://openplaylist.midnull.space"
+
+        # Формируем зависимые ссылки
+        self.REDIRECT_URI = f"{self.PROJECT_DOMAIN}/oauth-callback"
+        return self
 
 settings = Settings()  # type: ignore # Загружаем настройки при старте
