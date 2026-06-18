0e763f3eb6b0684601e7d683948e9997836a5918
DustGalaxy <andreinter903@gmail.com>
2026-06-18 17:54:57 +0300

    fix sio websockets

--- back-end/src/adapters/_sio/init.py
+++ back-end/src/adapters/_sio/init.py
@@ -4,9 +4,18 @@ from src.settings import settings
 
 mgr = socketio.AsyncRedisManager(url=f"{settings.REDIS_URL}/1")
 sio = socketio.AsyncServer(
-    async_mode="asgi", client_manager=mgr, cors_allowed_origins=[],
+    async_mode="asgi",
+    client_manager=mgr,
+    cors_allowed_origins=[
+        "https://openplaylist.midnull.space",  # Твой боевой домен
+        "https://openplaylist.localhost",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
 )
-sio.instrument(auth={
-    'username': 'admin',
-    'password': "admin123",
-})    
+sio.instrument(
+    auth={
+        "username": "admin",
+        "password": "admin123",
+    }
+)
